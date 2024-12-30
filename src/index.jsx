aa0546c7ac4a2c8bf275c4f5b476b7953274a66d
Ahmad Sadid Abdul Salam <sadid@Ahmad-Sadid-Abdul-Salam.local>
2024-12-29 22:42:13 -0800

    retesting jsx files 2.0

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "./style.css";
 
 export const DashboardCrm = () => {
   return (
     <div className="dashboard-CRM">
       <h1>Dashboard CRM</h1>
       <p>Welcome to the Dashboard!</p>
+      {/* Add more parts here gradually */}
     </div>
   );
 };
